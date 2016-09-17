350e2e5ea34fa438c6cb50a230fe04f6335ab31f
Easyhood <357802051@qq.com>
2016-09-17 17:36:42 +0800

    服务器端和客户端雏形

---
+++ ADTProject/src/com/rgk/serverclient/ServerClient.java
@@ -0,0 +1,61 @@
+package com.rgk.serverclient;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class ServerClient {
+	public static void main(String[] args) {
+		try {
+			final ServerSocket server = new ServerSocket(12345);
+			System.out.println("服务端打开"+new Date());
+			
+			new Thread(){
+				public void run() {
+					while(true){
+						try {
+							Socket socket = server.accept();
+							System.out.println("客户端"+socket+"连接进来了");
+							new MessageThread(socket).start();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					
+				};
+			}.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	static class MessageThread extends Thread{
+		Socket socket;
+		private DataInputStream inputStream;
+		public MessageThread(Socket socket) {
+			this.socket = socket;
+			try {
+				inputStream = new DataInputStream(socket.getInputStream());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		@Override
+		public void run() {
+			while(true){
+				try {
+					String result = inputStream.readUTF();
+					System.out.println(result);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
